5644b765f8bc1b36c990b6dd745401bc76c7affb
DzirDev <zerqzmc@gmail.com>
2022-01-13 10:12:28 -0500

    Small tweaks

--- src/main/java/listeners/ChatListener.java
+++ src/main/java/listeners/ChatListener.java
@@ -20,6 +20,7 @@ public class ChatListener implements Listener {
         String prefix = l.getLuckPerms().getUserManager().getUser(player.getUniqueId()).getCachedData().getMetaData().getPrefix();
         if(prefix != null){
             event.setFormat(ColourUtils.colour(prefix + player.getDisplayName() + " &8\u00BB &r" + event.getMessage()));
+            player.setPlayerListName(ColourUtils.colour(prefix + player.getDisplayName()));
         } else {
             event.setFormat(ColourUtils.colour("&r" + player.getDisplayName() + " &8\u00BB &r" + event.getMessage()));
         }
